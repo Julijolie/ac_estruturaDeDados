b16ebdb7bdba51aa4ce1cb1aac6ee90e7cdd89b5
Juliana de Oliveira <juliana.ibmec@gmail.com>
2023-08-17 13:23:19 -0300

    melhorias na switch case

--- ac2-estruturaDeDados/agenda.go
+++ ac2-estruturaDeDados/agenda.go
@@ -4,43 +4,89 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"strings"
 )
-
-func main() {
-	var contatos [5]Contato
-
-	for i := 0; i < len(contatos); i++ {
-		fmt.Println("Nome:")
-		nome, _ := leitor.ReadString('\n')
-
-		fmt.Println("E-mail:")
-		email, _ := leitor.ReadString('\n')
-
-		contato := Contato{
-			Nome:  nome,
-			Email: email,
-		}
-
-		contatos = addContato(contato, contatos)
-		fmt.Println("***************************************")
-	}
-
-	fmt.Println(contatos)
-}
-
-var leitor = bufio.NewReader(os.Stdin)
 
 type Contato struct {
 	Nome  string
 	Email string
 }
 
-func addContato(contato Contato, contatos [5]Contato) [5]Contato {
+func (c *Contato) AlterarEmail(novoEmail string) {
+	c.Email = novoEmail
+}
+
+func adicionaContato(contato Contato, contatos *[5]Contato) {
 	for i := range contatos {
 		if contatos[i] == (Contato{}) {
 			contatos[i] = contato
 			break
 		}
 	}
-	return contatos
 }
+
+func excluiContato(contatos *[5]Contato) {
+	for i := len(contatos) - 1; i >= 0; i-- {
+		if contatos[i] != (Contato{}) {
+			contatos[i] = Contato{}
+			break
+		}
+	}
+}
+
+func main() {
+	var contatos [5]Contato
+
+	for {
+		fmt.Println("Escolha uma opção:")
+		fmt.Println("1. Adicionar contato")
+		fmt.Println("2. Excluir último contato")
+		fmt.Println("3. Mostrar contatos")
+		fmt.Println("4. Sair")
+
+		var escolha int
+		fmt.Scanln(&escolha)
+
+		switch escolha {
+		case 1:
+			fmt.Println("Nome:")
+			nome, _ := leitor.ReadString('\n')
+			nome = strings.TrimSpace(nome)
+
+			fmt.Println("E-mail:")
+			email, _ := leitor.ReadString('\n')
+			email = strings.TrimSpace(email)
+
+			contato := Contato{
+				Nome:  nome,
+				Email: email,
+			}
+
+			adicionaContato(contato, &contatos)
+			fmt.Println("Contato adicionado!")
+
+		case 2:
+			excluiContato(&contatos)
+			fmt.Println("Último contato excluído!")
+
+		case 3:
+			fmt.Println("Lista de contatos:")
+			for _, c := range contatos {
+				if c != (Contato{}) {
+					fmt.Printf("Nome: %s\nE-mail: %s\n", c.Nome, c.Email)
+				}
+			}
+
+		case 4:
+			fmt.Println("Encerrando o programa...")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Opção inválida!")
+		}
+		fmt.Println("-------------------------------")
+	}
+}
+
+var leitor = bufio.NewReader(os.Stdin)
+
